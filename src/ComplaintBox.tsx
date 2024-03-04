5fa73fdf034cc1f87a05769ddcea241852663c60
dipankar-m <dipankar.m@surya-digital.com>
2024-03-04 10:54:56 +0530

    Remove unnecessary comments

--- src/ComplaintBox.tsx
+++ src/ComplaintBox.tsx
@@ -3,7 +3,6 @@ import { observer } from "mobx-react";
 import { Box, Container, TextField, Typography } from "@mui/material";
 import { useRootStore } from "./App";
 import { Button, Icon } from "@surya-digital/leo-reactjs-material-ui";
-// import { useStore } from "./store";
 
 export const ComplaintBox = observer((): React.ReactElement => {
   const {
@@ -19,21 +18,12 @@ export const ComplaintBox = observer((): React.ReactElement => {
     usersArr,
     handleSubmit,
   } = useRootStore();
-  // const [name, setName] = useState("");
-  // const [mail, setMail] = useState("");
-  // const [phone, setPhone] = useState("");
-  // const [complaint, setComplaint] = useState("");
-  // const [nameError, setNameError] = useState(false);
-  // const [mailError, setMailError] = useState(false);
-  // const [complaintError, setComplaintError] = useState(false);
 
   useEffect(() => {
     let retrievedName = window.localStorage.getItem("name");
     if (retrievedName) setName(retrievedName);
     let retrievedMail = window.localStorage.getItem("mail");
     if (retrievedMail) setMail(retrievedMail);
-    // let retrievedPhone = window.localStorage.getItem("phone");
-    // if (retrievedPhone) setPhone(retrievedPhone);
     let retrievedComplaint = window.localStorage.getItem("complaint");
     if (retrievedComplaint) setComplaint(retrievedComplaint);
   }, []);
@@ -44,14 +34,9 @@ export const ComplaintBox = observer((): React.ReactElement => {
   useEffect(() => {
     window.localStorage.setItem("mail", mail);
   }, [mail]);
-  // useEffect(() => {
-  //   window.localStorage.setItem("phone", phone);
-  // }, [phone]);
   useEffect(() => {
     window.localStorage.setItem("complaint", complaint);
   }, [complaint]);
-
-  let flag = 0;
 
   const complaintArray = usersArr.map((userComplaint) => (
     <li key={userComplaint.name}>
@@ -100,7 +85,6 @@ export const ComplaintBox = observer((): React.ReactElement => {
         <Typography sx={{ fontStyle: "italic" }}>
           Get assured resolution within 24 hours**
         </Typography>
-        {/* <form noValidate autoComplete="off" onSubmit={handleSubmit}> */}
         <TextField
           onChange={(e) => setName(e.target.value)}
           fullWidth
@@ -123,16 +107,6 @@ export const ComplaintBox = observer((): React.ReactElement => {
           variant="standard"
           error={mailError}
         />
-        {/* <TextField
-          fullWidth
-          onChange={(e) => setPhone(e.target.value)}
-          value={phone}
-          id="outlined-basic"
-          label="Phone Number"
-          variant="standard"
-          error={phoneError}
-          helperText="Enter your 10 digit phone number"
-        /> */}
         <TextField
           onChange={(e) => setComplaint(e.target.value)}
           fullWidth
@@ -147,20 +121,6 @@ export const ComplaintBox = observer((): React.ReactElement => {
           helperText="Enter your complaint in 50 characters."
         />
         <Box sx={{ display: "flex", justifyContent: "center", mt: 6 }}>
-          {/* <Button
-              type="submit"
-              variant="contained"
-              sx={{
-                borderRadius: "30px",
-                fontSize: "18px",
-                mt: 4,
-                mb: 4,
-                fontWeight: "bold",
-                textTransform: "none",
-              }}
-            >
-              Submit
-            </Button> */}
           <Button
             size={"large"}
             variant={"filled"}
@@ -171,7 +131,6 @@ export const ComplaintBox = observer((): React.ReactElement => {
             iconPosition="trailing"
           />
         </Box>
-        {/* </form> */}
         <Typography sx={{ fontSize: "12px" }}>
           **Excluding weekends and public holidays
         </Typography>
--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,4 @@
 import { types } from "mobx-state-tree";
-import { FormEvent } from "react";
 
 const validateMail = (str: string): boolean => {
   let atIndex = str.indexOf("@");
@@ -17,11 +16,9 @@ export const RootStore = types
   .model({
     name: types.string,
     mail: types.string,
-    // phone: types.string,
     complaint: types.string,
     nameError: types.boolean,
     mailError: types.boolean,
-    // phoneError: types.boolean,
     complaintError: types.boolean,
     usersArr: types.array(User),
   })
@@ -36,12 +33,7 @@ export const RootStore = types
       if (value && !validateMail(value)) this.setMailError(true);
       else this.setMailError(false);
     },
-    // setPhone(value: string) {
-    //   self.phone = value;
-    //   if (value.length !== 10 && value.length !== 0 && isNaN(parseInt(value)))
-    //     this.setPhoneError(true);
-    //   else this.setPhoneError(false);
-    // },
+
     setComplaint(value: string) {
       self.complaint = value;
       if (value.length > 50) this.setComplaintError(true);
@@ -53,9 +45,6 @@ export const RootStore = types
     setMailError(value: boolean) {
       self.mailError = value;
     },
-    // setPhoneError(value: boolean) {
-    //   self.phoneError = value;
-    // },
     setComplaintError(value: boolean) {
       self.complaintError = value;
     },
@@ -63,19 +52,12 @@ export const RootStore = types
       self.usersArr.push(value);
     },
     handleSubmit() {
-      // e.preventDefault();
-
-      // setNameError(false);
-      // setMailError(false);
-      // setComplaintError(false);
-
       console.log("handleSubmit called");
 
       if (self.name === "") this.setNameError(true);
       if (self.mail === "") this.setMailError(true);
       if (self.complaint === "" || self.complaint.length > 40)
         this.setComplaintError(true);
-      // if (phone.length != 10) setPhoneError(true);
 
       if (
         self.name &&
@@ -88,7 +70,6 @@ export const RootStore = types
         alert(
           `Complaint registered successfully. It will be resolved soon by our correspondent.`
         );
-        //saving data logic
         let name = self.name;
         let mail = self.mail;
         let complaint = self.complaint;
@@ -97,46 +78,7 @@ export const RootStore = types
 
         this.setName("");
         this.setMail("");
-        // this.setPhone("");
         this.setComplaint("");
       }
     },
   }));
-
-// let _store: {
-//   name: string;
-//   mail: string;
-//   phone: string;
-//   complaint: string;
-//   setName(value: string): void;
-//   setMail(value: string): void;
-//   setPhone(value: string): void;
-//   setComplaint(value: string): void;
-// };
-
-// export const useStore = () => {
-//   const store = useMemo(() => {
-//     if (!_store)
-//       _store = RootStore.create({
-//         name: "",
-//         mail: "",
-//         phone: "",
-//         complaint: "",
-//       });
-//     return _store;
-//   }, []);
-//   return store;
-// };
-
-// autorun(() => {
-//   window.localStorage.setItem("name", _store.name);
-// });
-// autorun(() => {
-//   window.localStorage.setItem("mail", _store.mail);
-// });
-// autorun(() => {
-//   window.localStorage.setItem("phone", _store.phone);
-// });
-// autorun(() => {
-//   window.localStorage.setItem("complaint", _store.complaint);
-// });
